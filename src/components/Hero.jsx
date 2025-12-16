e4f1c7fa877438813a0bd5bab74d71f3bff96268
Jonas Killich <youness.killich8@gmail.com>
2025-12-16 07:51:18 +0100

    Update profil section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Youness</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I'm a AI DevOps Engineer, <br className='sm:block hidden' />
+          I'm a System - DevOps Engineer, <br className='sm:block hidden' />
           passionate about automation, infrastructure, CI/CD and AI. Dedicated and ambitious in optimizing workflows and ensuring reliable software delivery.
 
           </p>
