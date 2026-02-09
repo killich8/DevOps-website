8c4920d9be64b456ceb01b37139ee609066c9279
Jonas Killich <youness.killich8@gmail.com>
2026-02-10 00:19:37 +0100

    Update the Overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,11 +22,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
     >
-        I am a professional MLOps Engineer focusing on Machine Learning Operations, automation, 
-        infrastructure, CI/CD, container orchestration and security. 
-        My strength lies in building scalable, reliable, and secure deployment pipelines. 
-        With a strong ability to quickly adapt and a practical mindset towards problem-solving, 
-        I work with teams in developing workflows that will allow them to ensure high performance and cost-effective software delivery.
+      I am an MLOps Engineer specializing in Machine Learning Operations, automation, cloud infrastructure,
+      CI/CD pipelines, container orchestration, and security. I focus on building scalable, reliable, and secure ML
+      systems from development to production. With a strong ability to adapt quickly and a hands-on problem-solving mindset,
+      I collaborate with cross-functional teams to design efficient workflows that improve
+      performance, reliability, and cost-effective software delivery.
     </motion.p>
 
    
